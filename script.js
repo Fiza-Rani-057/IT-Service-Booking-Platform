0e4a8c5ba954f538aa09c38d829f8b6c581671b5
Fiza-Rani-057 <fahadsaeed057@gmail.com>
2026-08-30 13:24:54 +0500

    Customer Support System

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
- const loginForm = document.getElementById("loginForm");
+const loginForm = document.getElementById("loginForm");
 const email = document.getElementById("email");
 const password = document.getElementById("password");
 const togglePassword = document.getElementById("togglePassword");
 
-loginForm.addEventListener("submit", function(event) {
+loginForm.addEventListener("submit", function (event) {
     event.preventDefault();
     if (email.value.trim() === "") {
         alert("Please enter your email");
@@ -11,10 +11,12 @@ loginForm.addEventListener("submit", function(event) {
         alert("Please enter your password");
     } else {
         alert("Login successful!");
+
+          window.location.href = "dashboard.html";
     }
 });
 
-togglePassword.addEventListener("click", function() {
+togglePassword.addEventListener("click", function () {
     if (password.type === "password") {
         password.type = "text";
     } else {
