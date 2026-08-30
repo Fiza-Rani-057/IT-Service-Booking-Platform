c49997ef00b6c8dc124c3245cede75670aa2b50a
Fiza-Rani-057 <fahadsaeed057@gmail.com>
2026-08-31 01:05:23 +0500

    Customer Support Service

--- script.js
+++ script.js
@@ -5,56 +5,35 @@ const togglePassword = document.getElementById("togglePassword");
 
 loginForm.addEventListener("submit", function (event) {
     event.preventDefault();
+
     if (email.value.trim() === "") {
         alert("Please enter your email");
-    } else if (password.value.trim() === "") {
+    } 
+    else if (password.value.trim() === "") {
         alert("Please enter your password");
-    } else {
-          window.location.href = "dashboard.html";
+    } 
+    else if (
+        email.value.trim() === "customer@gmail.com" &&
+        password.value.trim() === "customer123"
+    ) {
+        window.location.href = "dashboard.html";
+    } 
+    else if (
+        email.value.trim() === "provider@gmail.com" &&
+        password.value.trim() === "provider123"
+    ) {
+        window.location.href = "providerdb.html";
+    } 
+    else {
+        alert("Please enter correct email or password");
     }
 });
+
 togglePassword.addEventListener("click", function () {
     if (password.type === "password") {
         password.type = "text";
     } else {
         password.type = "password";
     }
-});
-
-
- 
-const searchInput = document.getElementById("searchInput");
-const categoryFilter = document.getElementById("categoryFilter");
-const searchBtn = document.getElementById("searchBtn");
-const providerCards = document.querySelectorAll(".provider-card");
-
-searchBtn.addEventListener("click", function () {
-    const searchValue = searchInput.value.toLowerCase().trim();
-    const selectedCategory = categoryFilter.value;
-
-    providerCards.forEach(function (card) {
-        const cardText = card.textContent.toLowerCase();
-        const cardCategory = card.getAttribute("data-category");
-
-        if ((searchValue === "" || cardText.includes(searchValue)) && (selectedCategory === "all" || cardCategory === selectedCategory)) {
-            card.style.display = "block";
-        } else {
-            card.style.display = "none";
-        }
-    });
-});
-
-categoryFilter.addEventListener("change", function () {
-    const selectedCategory = categoryFilter.value;
-
-    providerCards.forEach(function (card) {
-        const cardCategory = card.getAttribute("data-category");
-
-        if (selectedCategory === "all" || cardCategory === selectedCategory) {
-            card.style.display = "block";
-        } else {
-            card.style.display = "none";
-        }
-    });
 });
 
